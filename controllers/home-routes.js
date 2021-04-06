fcb50a10ea1a1eaa7983c52c6852f2df108262d3
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-04-06 05:35:42 -0500

    working on post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,52 @@
 const router = require('express').Router();
-const {User, Post, Comment} = require('../models')
+const { User, Post, Comment } = require('../models')
 
 //homepage
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     //get all post data
     const postData = await Post.findAll({
         include: [{
             model: User,
             attributes: ['id', 'user_name'],
-        },
-        {
-            model: Comment,
-            include: {
-                model: User,
-                attributes: ['id', 'user_name'],
-            }
         },]
     });
     //serialize that data
     const cleanPostData = postData.map(post => post.get());
-    res.render('homepage', {cleanPostData, userId: req.session.UserId})
+    res.render('homepage', { cleanPostData, userId: req.session.UserId })
 })
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
+        res.redirect('/');
+        return;
     }
-    res.render('login', {login: true});
-  });
+    res.render('login', { login: true });
+});
+
+router.get('/post/:id', async (req, res) => {
+    //check if user is logged in, if not redirect to login page
+    const postData = await Post.findByPk(req.params.id, {
+        include: [{
+            model: User,
+            attributes: ['id', 'user_name'],
+        },]
+    });
+    //serialize post data
+    const post = postData.get();
+    console.log(post)
+
+    const commentData = await Comment.findAll({
+        where: { post_id: post.id },
+        include: {
+            model: User,
+            attributes: ['id', 'user_name'],
+        },
+    })
+
+    const comments = commentData.map((comment) => comment.get())
+    console.log(comments)
+
+    res.render('postpage', { post, comments })
+})
 
 module.exports = router;
