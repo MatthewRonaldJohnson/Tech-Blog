cef97da56fc02b810820dbe551d5a85a70e93616
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-04-06 19:11:16 -0500

    added signup logic

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,84 +1,93 @@
 const router = require('express').Router();
-const {User} = require('../../models')
+const { User } = require('../../models')
 
-router.get('/', async (req,res) => {
-    const userData = await User.findAll({
-        attributes: ['id','user_name']
-    });
-    res.json(userData)
+router.get('/', async (req, res) => {
+  const userData = await User.findAll({
+    attributes: ['id', 'user_name']
+  });
+  res.json(userData)
 })
 
-router.get('/:id', async (req,res) => {
-    const userData = await User.findByPk(req.params.id,{
-        attributes: ['id','user_name']
-    });
-    if (userData === null) res.end('Not found')
-    res.json(userData)
+router.get('/:id', async (req, res) => {
+  const userData = await User.findByPk(req.params.id, {
+    attributes: ['id', 'user_name']
+  });
+  if (userData === null) res.end('Not found')
+  res.json(userData)
 })
 
-router.post('/', async (req,res) => {
-    const newUserData = await User.create(req.body)
-    res.json(newUserData)
+//create new user then sign them in
+router.post('/', async (req, res) => {
+  console.log('req.body: ',req.body)
+  const newUserData = await User.create(req.body);
+  console.log('newuserData: ',newUserData)
+  req.session.save(() => {
+    req.session.userId = newUserData.id;
+
+    res
+      .status(200)
+      .json({ user:newUserData, message: 'You are now logged in!' });
+  });
 })
 
 // /api/user/login
 router.post('/login', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
-        where: {
-          user_name: req.body.userName,
-        },
-      });
-  
-      if (!dbUserData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect user or password. Please try again!' });
-        return;
-      }
-  
-      const validPassword = await dbUserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect user or password. Please try again!' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.userId = dbUserData.id;
-  
-        res
-          .status(200)
-          .json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        user_name: req.body.userName,
+      },
+    });
 
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect user or password. Please try again!' });
+      return;
     }
-  });
 
-  // /api/user/logout
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect user or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = dbUserData.id;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+
+  }
+});
+
+// /api/user/logout
 router.post('/logout', (req, res) => {
-    if (req.session.userId) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+  if (req.session.userId) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
-router.put('/:id', async (req,res) => {
-    const updatedUserData = await User.update(req.body, {where: {id: req.params.id}})
-    res.json(updatedUserData)
+router.put('/:id', async (req, res) => {
+  const updatedUserData = await User.update(req.body, { where: { id: req.params.id } })
+  res.json(updatedUserData)
 })
 
-router.delete('/:id', async (req,res) => {
-    const deletedUser = await User.destroy({where: {id: req.params.id}})
-    res.json(deletedUser)
+router.delete('/:id', async (req, res) => {
+  const deletedUser = await User.destroy({ where: { id: req.params.id } })
+  res.json(deletedUser)
 })
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,55 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const userName = document.querySelector('#logUsername').value.trim();
-    const password = document.querySelector('#logPassword').value.trim();
-  
-    if (userName && password) {
-      const response = await fetch('/api/user/login', {
-        method: 'POST',
-        body: JSON.stringify({ userName, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {    //send them to homepage if login successfully
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in.');
-      }
+  event.preventDefault();
+
+  const userName = document.querySelector('#logUsername').value.trim();
+  const password = document.querySelector('#logPassword').value.trim();
+
+  if (userName && password) {
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify({ userName, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {    //send them to homepage if login successfully
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.message);
     }
-  };
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const userName = document.querySelector('#signUsername').value.trim();
+  const password = document.querySelector('#signPassword').value.trim();
+  const confirmPassword = document.querySelector('#confirmPassword').value.trim();
+
+  if (!(userName && password && confirmPassword)) {
+    alert('You must enter values into all 3 fields to sign up');
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert('Passwords do not match');
+    return;
+  }
+
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({ user_name: userName, password }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+
+};
 
 
-document.getElementById('logInForm').addEventListener('submit',loginFormHandler);
+document.getElementById('logInForm').addEventListener('submit', loginFormHandler);
+document.getElementById('signUpForm').addEventListener('submit', signupFormHandler);
